5865aa5fec94ac32bca9732017b96854071200e1
elmejor254 <104608101+elmejor254@users.noreply.github.com>
2022-04-29 11:11:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'obisoftdev'
+        tl_admin_user = 'demian2008'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5270983050:AAF9-0hePS70oBWP4bmCZaQZUXfOvyhI1hA'
+    bot_token = '5354489139:AAHRp9Vvdxp2QMY4sprIsn5-B2kj1Gk4cJs'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
